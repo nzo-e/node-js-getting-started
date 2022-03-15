f3744324b4f30896dba00894432fc23388b98fbc
(lor)enzo <lorenzoyanga@gmail.com>
2022-03-15 16:21:40 +0800

    change of steamapi implementation and db implementation

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { resolve } = require('path');
 
 const cool = require('cool-ascii-faces');
 
 const SteamAPI = require('steamapi');
-const { resolve } = require('path');
 const steam  = new SteamAPI('9C2F8B579B777036AD7B17C6A5BEC8FA')
 
-
-let teststring = "";
-
-steam.getUserOwnedGames("76561198133758253",true,true).then(arr=>{
-  let output = new String;
-  arr.forEach(x=>{
-    output += `[${x.name}, ${x.appID}, ${x.playTime}, ${x.playTime2}]\n`
-  })
-
-  //console.log(typeof(output));
-  //console.log(output);
-  teststring = output;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 express()
@@ -27,9 +21,36 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/test',(req,res)=>res.send(`${teststring}`))
+  .get('/test', async (req,res)=>{
+    try {
+      steam.getUserOwnedGames("76561198133758253",true,true).then(arr=>{
+        let output = new String;
+        arr.forEach(x=>{
+          output += `[${x.name}, ${x.appID}, ${x.playTime}, ${x.playTime2}]\n`
+        })
+        res.send(output)
+      });
+    }
+    catch (err)
+    {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 console.log('post-listen message');
