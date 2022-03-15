cc9c0d25df45cbe1be307ccba523f3958d48ff29
(lor)enzo <lorenzoyanga@gmail.com>
2022-03-15 14:20:48 +0800

    lots of testing with steamapi dependency

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ const PORT = process.env.PORT || 5000
 const cool = require('cool-ascii-faces');
 
 const SteamAPI = require('steamapi');
+const { resolve } = require('path');
 const steam  = new SteamAPI('9C2F8B579B777036AD7B17C6A5BEC8FA')
+
+
+let teststring = "";
+
+steam.getUserOwnedGames("76561198133758253",true,true).then(arr=>{
+  let output = new String;
+  arr.forEach(x=>{
+    output += `[${x.name}, ${x.appID}, ${x.playTime}, ${x.playTime2}]\n`
+  })
+
+  //console.log(typeof(output));
+  //console.log(output);
+  teststring = output;
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/test',(req,res)=>res.send(`${teststring}`))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+console.log('post-listen message');
